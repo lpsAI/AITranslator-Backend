3ddb13c8db933a20dbc48169edf1227ae6bb3fc4
Loven Sindol <JuanLoven.M.Sindol@pccw.com>
2024-02-06 10:04:22 +0800

    fix conversate return

--- controller/aiTranslationController.js
+++ controller/aiTranslationController.js
@@ -8,5 +8,5 @@
   export const conversate = async (req, res) => {
     const detected = await detectLanguage(req.body)
 
-    res.status(200).json({result: detected.data})
+    res.status(200).json({result: detected})
   }
