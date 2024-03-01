d961c0468b8a48a815a5061d2ccca7e3c4b410d3
Loven Sindol <JuanLoven.M.Sindol@pccw.com>
2024-03-01 14:49:23 +0800

    Remove Socket io

--- server.js
+++ server.js
@@ -2,17 +2,9 @@ import './env.js'
 import express from 'express';
 import aiRoutes from './routes/aiTranslationRoutes.js'
 import cors from 'cors';
-import {Server} from 'socket.io';
-import {createServer } from 'http'
 
 const app = express();
 const port = process.env.NODE_PORT;
-const server = createServer(app);
-const serverSocket = new Server(server, {
-  cors: {
-    origin: '*'
-  }
-});
 
 app.use(cors({
   origin: '*'
@@ -21,18 +13,6 @@ app.use(cors({
 app.use(cors())
 
 app.use(express.json());
-
-serverSocket.on('connection', (socket) => {
-  console.log('New user connected ' + socket.id);
-
-  socket.on('sendMessage', (message) => {
-    serverSocket.emit('message', message); // Broadcast the message to all connected clients
-  });
-
-  socket.on('disconnect', () => {
-    console.log('User disconnected');
-  });
-});
 
 app.use('/api', aiRoutes);
 
@@ -45,6 +25,6 @@ app.get('/healthcheck', (req, res) => {
 })
 
 // Start the server
-server.listen(port, () => {
+app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
