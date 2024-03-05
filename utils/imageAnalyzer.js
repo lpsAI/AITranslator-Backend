102839d4d6eb15fa44c5f8be39ccb8b96ac2e4c0
jbgaela <jbgaela@gmail.com>
2024-03-05 13:34:42 +0800

    Removed Axios from imageTranslation

--- utils/imageAnalyzer.js
+++ utils/imageAnalyzer.js
@@ -1,11 +1,17 @@
 import { createWorker, createScheduler } from 'tesseract.js';
 
 const imageAnalyzer = async (url, language) => {
-    const worker = await createWorker(language);
-    const { data: { text } } = await worker.recognize(url);
-    await worker.terminate();
-
-    return text;
+    try {
+        const worker = await createWorker(language);
+        const {
+          data: { text },
+        } = await worker.recognize(url);
+        await worker.terminate();
+        return text;
+    } catch (error) {
+        console.error(error.message)
+    }
+    
 }
 
 export default imageAnalyzer
--- utils/imageAnalyzerUtils.js
+++ utils/imageAnalyzerUtils.js
@@ -1,9 +1,9 @@
 import { fileUpload } from "../controller/aiBlobStorageController.js";
-import axios from "axios";
 import imageAnalyzer from "../utils/imageAnalyzer.js";
+import { azureTranslation } from "../utils/azureAiTranslatorUtils.js";
 
 export default async function handleImageTranslation(req, res) {
-  let response 
+  let response;
   try {
     const { resJson, jsonBody } = await fileUpload(req);
 
@@ -13,21 +13,18 @@ export default async function handleImageTranslation(req, res) {
 
     const detectedText = await imageAnalyzer(url, originLanguage);
 
-    const translatedTextResponse = await axios.post(
-      process.env.BASE_URL + process.env.TRANSLATE_URL,
-      {
-        text: detectedText.replace(/\n/g, ""),
-        language: jsonBody.toLanguage,
-      }
-    );
-
-    const translatedText = translatedTextResponse.data;
-
-    response = {
-      imageUrl: url,
-      detectedText: detectedText.replace(/\n/g, ""),
-      translatedText: translatedText,
-    };
+    if (jsonBody.fromLanguage === "eng") {
+      const translationTextResponse = await azureTranslation(
+        detectedText.replace(/\n/g, ""),
+        jsonBody.toLanguage,
+        "en"
+      );
+      response = {
+        imageUrl: url,
+        detectedText: detectedText.replace(/\n/g, ""),
+        translationTextResponse: translationTextResponse[0].translations,
+      };
+    }
 
     return response;
   } catch (error) {
